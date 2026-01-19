27fa0a06d93d90ca192b6462e6547c0f56019c9c
juancarlospizarro <jcpizarroalonso@gmail.com>
2026-01-19 10:33:35 +0100

    Añadida la dependencia de JPA. Entidad Equipo creada.

--- tareaRA3/src/main/java/com/crudmvc/tarea3/entidad/Equipo.java
+++ tareaRA3/src/main/java/com/crudmvc/tarea3/entidad/Equipo.java
@@ -1,5 +1,80 @@
 package com.crudmvc.tarea3.entidad;
 
-public class Equipo {
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Equipos")
+public class Equipo {
+	   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+    private int anio_fundacion;
+    private String ciudad;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+	public int getAnio_fundacion() {
+		return anio_fundacion;
+	}
+
+
+	public void setAnio_fundacion(int anio_fundacion) {
+		this.anio_fundacion = anio_fundacion;
+	}
+
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+	    if (this == obj) {
+	        return true;
+	    }
+	    if (obj == null || getClass() != obj.getClass()) {
+	        return false;
+	    }
+	    Equipo other = (Equipo) obj;
+	    return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public int hashCode() {
+	    return Objects.hash(id);
+	}
 }
