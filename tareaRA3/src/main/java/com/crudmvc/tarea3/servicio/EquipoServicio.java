e86db0ef0a5ff560f8c4d620a4e90f2a0b3236db
juancarlospizarro <jcpizarroalonso@gmail.com>
2026-01-19 10:42:06 +0100

    Clase EquipoServicio modificada.

--- tareaRA3/src/main/java/com/crudmvc/tarea3/servicio/EquipoServicio.java
+++ tareaRA3/src/main/java/com/crudmvc/tarea3/servicio/EquipoServicio.java
@@ -1,5 +1,26 @@
 package com.crudmvc.tarea3.servicio;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crudmvc.tarea3.entidad.Equipo;
+import com.crudmvc.tarea3.repositorio.EquipoRepositorio;
+
+@Service
 public class EquipoServicio {
+
+    @Autowired
+    private EquipoRepositorio equipoRepositorio;
+
+
+    public Equipo guardarEquipo(Equipo equipo) {
+        return equipoRepositorio.save(equipo);
+    }
+
+	public List<Equipo> obtenerTodos() {
+		return equipoRepositorio.findAll();
+	}
 
 }
